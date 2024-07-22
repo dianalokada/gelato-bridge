e164a4b6b9ed9ee87fd583fcd655e2ad75782298
Diana <dianaalokhina@gmail.com>
2024-07-22 14:10:35 -0400

    delete web3functioncontext implementation

--- web3Function/bridge/index.ts
+++ web3Function/bridge/index.ts
@@ -18,23 +18,10 @@ const relay = new GelatoRelay();
 
 // Define the main Web3Function
 export const onRun = async (context: Web3FunctionEventContext): Promise<Web3FunctionResult> => {
-  // async function retryRelay(relayFunction, maxRetries = 3) {
-  //   for (let i = 0; i < maxRetries; i++) {
-  //     try {
-  //       return await relayFunction();
-  //     } catch (error) {
-  //       if (error.message.includes('Too many requests') && i < maxRetries - 1) {
-  //         console.log(`Rate limited, retrying in ${2 ** i} seconds...`);
-  //         await new Promise((resolve) => setTimeout(resolve, 1000 * 2 ** i));
-  //       } else {
-  //         throw error;
-  //       }
-  //     }
-  //   }
-  // }
 
   console.log('Starting onRun function');
   const { userArgs, secrets, log, gelatoArgs } = context;
+  if(log){
    // Parse your event from ABI
    const token = new Interface(ABI);
    const event = token.parseLog(log);
@@ -87,111 +74,10 @@ export const onRun = async (context: Web3FunctionEventContext): Promise<Web3Func
   );
 
   return { canExec: false, message: "Processed events and relayed transactions" }
+} else {
+  console.log('No log data available');
+  return { canExec: false, message: 'No log data available' };
 }
-  
-
-  // // Get the dedicated address from secrets or user args
-  // const dedicatedAddress =
-  //   (await secrets.get('DEDICATED_ADDRESS')) || (userArgs.dedicatedAddress as string);
-  // if (!dedicatedAddress) {
-  //   return { canExec: false, message: 'Dedicated address not set in secrets or user args' };
-  // }
-  // console.log('Dedicated address:', dedicatedAddress);
-
-//   // Validate that required configuration variables are defined
-//   if (!contractAddressArbitrumSepolia) {
-//     throw new Error('Arbitrum Sepolia contract address is not defined in userArgs');
-//   }
-
-//   if (!contractAddressOptimismSepolia) {
-//     throw new Error('Optimism Sepolia contract address is not defined in userArgs');
-//   }
-
-//   try {
-//     // Create providers for Arbitrum and Optimism Sepolia networks
-//     const arbitrumProvider = new ethers.JsonRpcProvider(arbitrumSepoliaRPC);
-//     const optimismProvider = new ethers.JsonRpcProvider(optimismSepoliaRPC);
-
-//     // Create contract instances
-//     const arbitrumContract = new Contract(
-//       contractAddressArbitrumSepolia as string,
-//       ABI,
-//       arbitrumProvider,
-//     );
-//     const optimismContract = new Contract(
-//       contractAddressOptimismSepolia as string,
-//       ABI,
-//       optimismProvider,
-//     );
-
-//     const processEvents = async (
-//       sourceContract: Contract, // The contract to listen for events on
-//       targetContract: Contract, // The target Contract
-//       targetChainId: bigint, // The chain ID of the target network
-//       targetAddress: string, // The address of the target contract
-//     ) => {
-//       console.log('Processing events for chain ID:', targetChainId.toString());
-//       const startBlock = -10000;
-//       // Create a filter for TokensBurned events
-//       const filter = sourceContract.filters.TokensBurned();
-//       // Queries the last blockchain for TokensBurned events
-//       const events = await sourceContract.queryFilter(filter, startBlock, 'latest');
-//       console.log('Number of events found:', events.length);
-//       //when the transaction is detected, the web3function processes it and triggers a  mint on the other chain
-//       if (events.length > 0) {
-//         // Sort events by block number in descending order
-//         const sortedEvents = events.sort((a, b) => b.blockNumber - a.blockNumber);
-
-//         // Get the most recent event
-//         const latestEvent = sortedEvents[0];
-
-//         if (latestEvent instanceof EventLog && latestEvent.args) {
-//           const [from, amount] = latestEvent.args;
-//           const mintCalldata = targetContract.interface.encodeFunctionData(
-//             'mintViaDedicatedAddress',
-//             [from, amount],
-//           );
-
-//           try {
-//             //the ttransaction is relayed to the target chain using gelato's sponsoredcall
-//             console.log('Calling relay.sponsoredCall');
-//             const relayResponse = await retryRelay(() =>
-//               relay.sponsoredCall(
-//                 {
-//                   chainId: targetChainId,
-//                   target: targetAddress,
-//                   data: mintCalldata,
-//                 },
-//                 gelatoApiKey,
-//               ),
-//             );
-//             console.log(`Relay response: ${relayResponse.taskId}`);
-//           } catch (error) {
-//             console.error('Error relaying transaction:', error);
-//           }
-//         }
-//       }
-//     };
-//     //the function processevents is called for both chains,making sure that a burn on one chain triggers a mint on the other chain 
-//     await processEvents(
-//       arbitrumContract,
-//       optimismContract,
-//       BigInt(11155420),
-//       contractAddressOptimismSepolia as string,
-//     ); // Optimism Sepolia chain ID
-//     await processEvents(
-//       optimismContract,
-//       arbitrumContract,
-//       BigInt(421614),
-//       contractAddressArbitrumSepolia as string,
-//     ); // Arbitrum Sepolia chain ID
-
-//     return { canExec: false, message: 'Processed events and relayed transactions' };
-//   } catch (error) {
-//     console.error('Error in Web3 Function:', error);
-//     return { canExec: false, message: `Error occurred: ${(error as Error).message}` };
-//   }
-// };
-
+}
 //pass the onRun function to the Web3Function.onRun method
 Web3Function.onRun(onRun);
